f264ca1fcb68365e918198fe63bafb83b038c7a9
Blas Gomez <jblas1989@gmail.com>
2021-08-20 09:12:11 +0100

    fancybox buttons not showing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 // JS Goes here - ES6 supported
 
-import "./css/main.css";
+import "./js/app";
+import "./js/backtotop";
+import "./js/util";
+import { Fancybox } from "@fancyapps/ui";
 
 var cloudinary = require('cloudinary-core');
 var cl = cloudinary.Cloudinary.new();
 cl.responsive();
+
+// $.fancybox.defaults.buttons =
+// [
+// "zoom",
+// //"share",
+// "slideShow",
+// "fullScreen",
+// //"download",
+// "thumbs",
+// "close"
+// ];
