c11e3043ba038d92f417c81582eb36302035dcd9
Blas Gomez <jblas1989@gmail.com>
2021-08-20 08:23:48 +0100

    tidy up

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@
 
 import "./css/main.css";
 
-// Say hello
-/* eslint no-console: ["error", { allow: ["log"] }] */
 var cloudinary = require('cloudinary-core');
 var cl = cloudinary.Cloudinary.new();
 cl.responsive();
-
-// var tag = cloudinary.ImageTag.new("sample", {cloud_name: "demo", width: 300});
-// document.body.appendChild(tag.toDOM());
-// document.body.appendChild(
-//     tag.transformation()
-//         .angle(15)
-//         .width(200)
-//         .crop("scale")
-//         .effect("grayscale")
-//         .getParent().toDOM()
-// );
