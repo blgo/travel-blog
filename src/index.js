a0f7f395c9a2a1a6e4d8d3afddbaa648db2a0d57
blgo <jblas1989@gmail.com>
2021-12-12 19:00:37 +0000

    Update dependencies and bring boilerplate code up to date (#206)

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// JS Goes here - ES6 supported
+
+import "./js/app";
+import "./js/backtotop";
+import "./js/util";
+import { Fancybox } from "@fancyapps/ui";
+// import hljs from "highlight.js";
+
+var cloudinary = require('cloudinary-core');
+var cl = cloudinary.Cloudinary.new();
+cl.responsive();
+// hljs.initHighlightingOnLoad();
+
+// $.fancybox.defaults.buttons =
+// [
+// "zoom",
+// //"share",
+// "slideShow",
+// "fullScreen",
+// //"download",
+// "thumbs",
+// "close"
+// ];
